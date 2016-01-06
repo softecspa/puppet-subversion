ff1a5cf4bec6711b3030699d57586ecb42ba03a8
lorello <lorenzo.salvadorini@softecspa.it>
2016-01-06 12:16:07 +0100

    added fixed packages for Hardy, PPA for Lucid & Precise, simple package for Trusty and newer

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,41 @@
 # Class: subversion
 #
 # Manage subversion checkouts
-class subversion {
+class subversion
+{
 
-  if $::lsbdistcodename == 'hardy' {
-
-    $pkg = "libneon27-gnutls_0.27.2-1ubuntu0.1_${::architecture}.deb"
-    $pkg_name = 'libneon27-gnutls'
-
-    package { 'subversion':
-      ensure  => latest,
-      require => Exec['install_libneon']
+  case $::lsbdistcodename {
+    'hardy':
+    {
+      $pkg = "libneon27-gnutls_0.27.2-1ubuntu0.1_${::architecture}.deb"
+      $pkg_name = 'libneon27-gnutls'
+      exec{'install_libneon':
+        cwd     => '/tmp',
+        command => "wget http://tools.softecspa.it/${pkg} && dpkg -i ${pkg} && rm ${pkg}",
+        unless  => "dpkg -l ${pkg_name} | grep ^ii"
+      }->
+      package { 'subversion':
+        ensure  => latest,
+      }
     }
 
-    exec{'install_libneon':
-      cwd     => '/tmp',
-      command => "wget http://tools.softecspa.it/${pkg} && dpkg -i ${pkg} && rm ${pkg}",
-      unless  => "dpkg -l ${pkg_name} | grep ^ii"
+    /^(lucid|precise)$/:
+    {
+      softec_apt::ppa { 'svn/ppa':
+        mirror  => true,
+        key     => 'A2F4C039'
+      }->
+      package { 'subversion':
+        ensure  => latest,
+      }
+    }
+
+    default:
+    {
+      package { 'subversion':
+        ensure  => latest,
+      }
     }
   }
-  else {
-    package { 'subversion':
-      ensure  => latest,
-    }
-  }
+
 }
